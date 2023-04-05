113ec7620492362fc49db55f6a734f8e33a3a90d
Kyle Watson <kyle.p.watson@gmail.com>
2023-04-05 13:46:50 +0800

    Import of contact images and information

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 export default function About() {
     return (
         <div>
-            <h4>About</h4>
+            <h4 className='aboutHeader'>About</h4>
             <p className='about-blurb'>
-                An individual passionate about new innovations in technology and continually looking for new opportunities to build upon my existing knowledge and experiences.
+                Passionate about new innovations in technology and continually looking for new opportunities to build upon my existing knowledge and experiences.
             </p>
         </div>
     )
--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -1,17 +1,30 @@
 // Image, name, title, 
 import React from 'react';
-
+import linkedinApp from '../images/linkedin-app.png';
+import emailApp from '../images/email-app.png';
+import phoneApp from '../images/phone-app.png';
 export default function Info() {
     return (
         <div className='content'>
             <h1><span>Kyle</span> Watson</h1>
 
             <h4 className='info-role'>Developer</h4>
-            <p className='contact-information'>
-                +61 424466010 <br />
-                /in/kyle-watson-dev <br />
-                kyle.p.watson@gmail.com<br />
-            </p>
+
+            <div>
+                <p>
+                    <img className="phone-app" src={phoneApp} />
+                    +61 424466010
+                </p>
+                <p>
+                    <img className="linkedin-app" src={linkedinApp} />
+                    /in/kyle-watson-dev
+                </p>
+                <p>
+                    <img className="email-app" src={emailApp} />
+                    kyle.p.watson@gmail.com
+                </p>
+            </div>
+
         </div>
     )
 
